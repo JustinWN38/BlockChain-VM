edd26135f4c20f4fd95cc2ac7c0e6cdf0e289ecf
ian <ian@evm.myguest.virtualbox.org>
2024-11-29 12:10:01 +0000

    Token1.js

--- test/Token1.js
+++ test/Token1.js
@@ -1,3 +1,4 @@
+const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { expect } = require("chai");
 
 describe("Token contract 1", function () {
@@ -16,11 +17,34 @@ describe("Token contract 1", function () {
     const hardhatToken = await ethers.deployContract("Token");
 
     await hardhatToken.transfer(addr1.address, 500);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(500);
+    expect( await hardhatToken.balanceOf(addr1.address)).to.equal(500);
     let amount=100;
     await hardhatToken.connect(addr1).transfer(addr2.address, amount);
-    expect(await hardhatToken.balanceOf(addr1.address)).to.equal(400);
+    expect( await hardhatToken.balanceOf(addr1.address)).to.equal(400);
     console.log(amount)
-    expect(await hardhatToken.balanceOf(addr2.address)).to.equal(amount);
+    expect( await hardhatToken.balanceOf(addr2.address)).to.equal(amount);
   })
 });
+
+describe("Transcation", function(){
+    it("owner transfer", async function() {
+        const {hardhatToken, addr1} = await loadFixture(deployTokenFixture);
+        const _amount = 1000;
+        expect ( await hardhatToken.transfer(addr1, _amount)).to.changeTokenBalance(hardhatToken, addr1, _amount);
+    })
+    it("address transfer", async function(){ 
+        const {hardhatToken, addr1, addr2} = await loadFixture(deployTokenFixture);
+        const _amount = 1000;
+        expect ( await hardhatToken.transfer(addr1, _amount)).to.changeTokenBalance(hardhatToken, addr1, _amount);
+        expect ( await hardhatToken.connect(addr1).transfer(addr2.address, _amount)).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-_amount, + _amount]);
+    })
+
+})
+
+describe("Revert", function(){
+    it("insufficient funds", async function(){
+        const {hardhatToken, addr1, addr2} = await loadFixture(deployTokenFixture);
+        const _amount = 1000;
+        expect ( await hardhatToken.connect(addr1).transfer(addr2.address, _amount)).to.be.revertedWith(**)
+    })
+})
